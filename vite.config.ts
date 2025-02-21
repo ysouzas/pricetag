947b074a7751332caa443a737783832a30d91e6d
Yago Souza Santos <6200092+ysouzas@users.noreply.github.com>
2025-02-21 12:10:53 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-});
+import { loadEnv } from "vite";
+
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  process.env = { ...process.env, ...env };
+  return defineConfig({
+    plugins: [react()],
+  });
+};

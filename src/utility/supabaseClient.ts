6305dbaf469f02451a850bc9f8cb1685892f51aa
Yago Souza Santos <6200092+ysouzas@users.noreply.github.com>
2025-02-21 11:27:33 +0000

    Update supabaseClient.ts

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,14 +1,16 @@
 import { createClient } from "@refinedev/supabase";
 
-export const supabaseClient = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-  {
-    db: {
-      schema: "public",
-    },
-    auth: {
-      persistSession: true,
-    },
-  }
-);
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+console.log(SUPABASE_URL);
+console.log(SUPABASE_ANON_KEY);
+
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  db: {
+    schema: "public",
+  },
+  auth: {
+    persistSession: true,
+  },
+});
